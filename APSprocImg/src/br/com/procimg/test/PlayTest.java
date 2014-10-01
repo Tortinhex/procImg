e14a701cb55035236a4a5264d952df02e01364e7
danilo <danilo@localhost>
2014-10-01 12:56:32 -0300

    Inicializaçao do repositorio

---
+++ APSprocImg/src/br/com/procimg/test/PlayTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.procimg.test;
+
+/**
+ *
+ * @author danilo
+ */
+public class PlayTest {
+    public static void main(String[] args){
+        System.out.println("Sejam bem vindos");
+    }
+}
